e199fbfc4395f3f95aba95554807e00b27289e74
VascoTeotonioPereira <vascotp2@gmail.com>
2021-07-02 09:58:41 +0100

    Order models

---
+++ SuperShop/Data/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SuperShop.Data.Entities;
+
+namespace SuperShop.Data
+{
+    public interface IOrderRepository : IGenericRepository<Order>
+    {
+        Task<IQueryable<Order>> GetOrderAsync(string userName);
+    }
+}
