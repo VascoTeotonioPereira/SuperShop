40134c835a154f69ad28782200162d671c89e439
VascoTeotonioPereira <vascotp2@gmail.com>
2021-07-08 09:24:32 +0100

    Confirm orders

--- SuperShop/Data/IOrderRepository.cs
+++ SuperShop/Data/IOrderRepository.cs
@@ -18,5 +18,7 @@ namespace SuperShop.Data
         Task ModifyOrderDetailTempQuantityAsync(int id, double quantity);
 
         Task DeleteDetailTempAsync(int id);
+
+        Task<bool> ConfirmOrderAsync(string userName);
     }
 }
